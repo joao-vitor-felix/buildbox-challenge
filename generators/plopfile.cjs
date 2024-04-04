f2f0a2cc3899d09f70ce30864f6fde87686e9928
João Vitor <joaovitorfelixcontato@gmail.com>
2024-04-04 15:05:08 -0300

    chore: initial commit

---
+++ generators/plopfile.cjs
@@ -0,0 +1,33 @@
+module.exports = function (plop) {
+  plop.setGenerator("component", {
+    description: "application component",
+
+    // inquirer prompts
+    prompts: [
+      {
+        type: "input",
+        name: "path",
+        message: "component path?",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "component name?"
+      },
+    ],
+
+    // actions to perform
+    actions: [
+      {
+        type: "add",
+        path: "../src/{{path}}/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "templates/component.tsx.hbs"
+      },
+      {
+        type: "add",
+        path: "../src/{{path}}/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        templateFile: "templates/component.test.tsx.hbs"
+      }
+    ]
+  });
+};
