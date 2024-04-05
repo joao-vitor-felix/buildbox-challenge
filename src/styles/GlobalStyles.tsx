81f09524909a2152d988e166988313cb4e393433
João Vitor <joaovitorfelixcontato@gmail.com>
2024-04-04 22:17:06 -0300

    feat: add GlobalStyles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+  height: 100%;
+}
+
+body {
+  box-sizing: border-box;
+  font-family: "Manrope", sans-serif;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.layout.background};
+}
+
+#root {
+  height: 100%;
+}
+`;
