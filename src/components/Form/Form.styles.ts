b0516d816e489745efdf12c9e1f2178a95224985
João Vitor <joaovitorfelixcontato@gmail.com>
2024-04-07 18:38:45 -0300

    refactor: add react hook form to Form

--- src/components/Form/Form.styles.ts
+++ src/components/Form/Form.styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   display: flex;
+  gap: 1.6rem;
   flex-direction: column;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.form.background};
@@ -20,4 +21,25 @@ export const InputGroup = styled.div`
   height: 100%;
   width: 100%;
   justify-content: center;
+`;
+
+export const Error = styled.span`
+  color: red;
+  font-size: 1.1rem;
+`;
+
+export const ResetFormButton = styled.button`
+  width: 6rem;
+  height: 1.7rem;
+  text-decoration: underline;
+  color: ${({ theme }) => theme.colors.layout.disabled};
+  background-color: transparent;
+  border: none;
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  align-self: flex-end;
+  align-items: center;
+  gap: 2.4rem;
 `;
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,66 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
 import { PhotoInput } from "../PhotoInput/PhotoInput";
 import * as S from "./Form.styles";
 
+const schema = z.object({
+  image: z.string().url({ message: "A Imagem é obrigatória." }),
+  name: z.string().trim().min(1, { message: "O nome é obrigatório." }),
+  message: z.string().trim().min(1, { message: "A mensagem é obrigatório." })
+});
+
+export type FormValues = z.infer<typeof schema>;
+
 export const Form = () => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      image: "",
+      name: "",
+      message: ""
+    }
+  });
+
+  const isFormNotValid = !form.formState.isValid || !form.formState.isDirty;
+
+  const handleResetForm = () => {
+    form.reset({
+      image: "",
+      name: "",
+      message: ""
+    });
+  };
+
+  const onSubmit = () => {};
+
   return (
-    <S.Form>
-      <PhotoInput />
+    <S.Form onSubmit={form.handleSubmit(onSubmit)}>
+      <PhotoInput formProps={form.register("image")} form={form} />
+      <S.Error>{form.formState.errors?.image?.message}</S.Error>
       <S.InputGroup>
-        <Input placeholder="Digite seu nome" />
-        <Input placeholder="Mensagem" inputType="TEXTAREA" as="textarea" />
+        <Input
+          placeholder="Digite seu nome"
+          formProps={form.register("name")}
+        />
+        <S.Error>{form.formState.errors?.name?.message}</S.Error>
+        <Input
+          placeholder="Mensagem"
+          inputType="TEXTAREA"
+          as="textarea"
+          formProps={form.register("message")}
+        />
+        <S.Error>{form.formState.errors?.message?.message}</S.Error>
       </S.InputGroup>
+      <S.ButtonGroup>
+        <S.ResetFormButton type="button" onClick={handleResetForm}>
+          Descartar
+        </S.ResetFormButton>
+        <Button disabled={isFormNotValid}>Publicar</Button>
+      </S.ButtonGroup>
     </S.Form>
   );
 };
