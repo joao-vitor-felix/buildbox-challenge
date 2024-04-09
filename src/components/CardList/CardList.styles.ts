6d4dda9db04074c5c77820edd56922a97174f921
João Vitor <joaovitorfelixcontato@gmail.com>
2024-04-09 11:31:54 -0300

    style: add media queries to CardList

--- src/components/CardList/CardList.styles.ts
+++ src/components/CardList/CardList.styles.ts
@@ -4,12 +4,17 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 5.6rem;
+
+  @media screen and (min-width: 768px) {
+    align-self: center;
+  }
 `;
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.6rem;
+  margin-bottom: 1.6rem;
 `;
 
 export const Header = styled.h3`
